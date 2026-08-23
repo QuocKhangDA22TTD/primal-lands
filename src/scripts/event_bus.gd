70bd3c6a472df21f74b1a076d95d4f2368793744
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-08-23 14:32:36 +0700

    Cập nhập script event_bus.

--- src/scripts/event_bus.gd
+++ src/scripts/event_bus.gd
@@ -1,2 +1,7 @@
-# EventBus.gd
+# event_bus.gd
 extends Node
+
+# Định nghĩa các tín hiệu yêu cầu chuyển chế độ chơi
+signal request_back_to_menu
+signal request_start_singleplayer
+# signal request_start_multiplayer(host_ip, port)
