55b50a858f02450a0b5a5271d77c09af694f85ab
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-08-23 14:33:53 +0700

    Tạo script main_menu và scene MainMenu.

---
+++ src/scripts/main_menu.gd
@@ -0,0 +1,16 @@
+# MainMenu.gd
+extends Control
+
+@onready var btn_start: Button = $VBoxContainer/StartButton
+@onready var btn_quit: Button = $VBoxContainer/QuitButton
+
+func _ready() -> void:
+	btn_start.pressed.connect(_on_start_pressed)
+	btn_quit.pressed.connect(_on_quit_pressed)
+
+func _on_start_pressed() -> void:
+	# Chỉ phát tín hiệu, không quan tâm ai sẽ xử lý
+	EventBus.request_start_singleplayer.emit()
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
